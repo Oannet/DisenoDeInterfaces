ce6f11ea50b5dad06674a5ac165a2cc3f902941a
Kevin-Antonio-Carvajal <kevin-antonio@ciencias.unam.mx>
2024-08-29 13:54:54 -0600

    13

--- Tarea2/script.js
+++ Tarea2/script.js
@@ -1,8 +1,14 @@
 document.getElementById('addTaskBtn').addEventListener('click', addTask);
 document.getElementById('taskInput').addEventListener('input', function() {
-    this.style.height = 'auto'; // Resetea la altura a automática para permitir reducción
+    this.style.height = 'auto'; 
     this.style.height = (this.scrollHeight) + 'px'; // Ajusta altura basada en el contenido
 });
+
+/**
+ * Agrega una nueva tarea a la lista de tareas.
+ * Crea un elemento de lista, lo configura con los botones necesarios y lo añade.
+ * También limpia el campo de entrada y actualiza los contadores de tareas.
+ */
 
 function addTask() {
     const taskInput = document.getElementById('taskInput');
@@ -15,7 +21,7 @@ function addTask() {
     if (taskName !== '') {
         const taskItem = document.createElement('li');
         taskItem.className = 'list-group-item task-item';
-        taskItem.innerHTML = taskName; // Usar innerHTML para interpretar el <br>
+        taskItem.innerHTML = taskName; 
         taskItem.addEventListener('click', () => toggleTaskCompleted(taskItem));
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Eliminar';
@@ -29,6 +35,10 @@ function addTask() {
     }
 }
 
+/**
+ * Ajusta el tamaño del contenedor de la lista de tareas para que se adapte
+ * a la altura de todos los elementos de tarea visibles.
+ */
 function adjustListSize() {
     const taskList = document.getElementById('taskList');
     let maxHeight = 0;
@@ -40,21 +50,36 @@ function adjustListSize() {
 
 // Llamar a adjustListSize cada vez que se añada o se elimine una tarea
 document.getElementById('addTaskBtn').addEventListener('click', () => {
-    setTimeout(adjustListSize, 100); // Asegura que se ajuste después de que la DOM haya sido actualizada
+    setTimeout(adjustListSize, 100); 
 });
 
+/**
+ * Elimina un elemento de tarea específico.
+ * También actualiza los contadores y ajusta el tamaño del contenedor de la lista de tareas.
+ * @param taskItem El elemento de la tarea a eliminar.
+ */
 function removeTask(taskItem) {
     const taskList = document.getElementById('taskList');
     taskList.removeChild(taskItem);
     updateCounters();
-    adjustListSize(); // Asegura que el tamaño del contenedor se ajuste después de eliminar una tarea
+    adjustListSize(); 
 }
 
+/**
+ * Alterna el estado de completado de una tarea.
+ * Cambia la clase del elemento para reflejar su estado de completado o no completado.
+ * También actualiza los contadores de tareas.
+ * @param taskItem El elemento de la tarea a alterar.
+ */
 function toggleTaskCompleted(taskItem) {
     taskItem.classList.toggle('task-completed');
     updateCounters();
 }
 
+/**
+ * Actualiza los contadores de tareas totales y completadas en la interfaz de usuario.
+ * Recuenta y muestra el número de tareas totales y el número de tareas completadas.
+ */
 function updateCounters() {
     const totalTasks = document.querySelectorAll('#taskList .task-item').length;
     const completedTasks = document.querySelectorAll('#taskList .task-completed').length;
